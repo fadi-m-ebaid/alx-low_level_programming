d37f78b39fc0526ed1489a806d45444dd7934329
fadi-m-ebaid <fadi.micheah@hotmail.com>
2022-12-10 14:53:54 -0800

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - main returns the last digit of an integer
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int num;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	num = n % 10;
+	
+	if (num > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, num);
+	}
+	else if (num == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, num);
+	}
+	else if (num < 6 && num != 0)
+	{
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, num);
+	}
+	else
+	{
+		printf("last digit of %d is %d\n", n, num);
+	}
+
+				
+	return (0);
+}
