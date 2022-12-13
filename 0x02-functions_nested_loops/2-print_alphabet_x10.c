17ff841b233b0c64d98011734e84da0115753370
fadi-m-ebaid <fadi.micheal@hotmial.com>
2022-12-13 13:26:50 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main"
+
+/**
+* main - prints 10 times the alphabet, in lowercase
+*
+* Return: a-z 10 times
+*/
+void print_alphabet_x10(void);
+{
+	int num = 0;
+	char c;
+
+	while (num <= 9);
+	{
+		c = 'a';
+		while (c <= 'z');
+		{
+			_putchar(c);
+			c++;
+		}
+		num++;
+		_putchar('\n');
+	}
+}
+
