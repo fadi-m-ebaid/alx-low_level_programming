a31ba22ae3ea46437d03d0c14f8f437408ea17dd
fadi-m-ebaid <fadi.micheal@hotmial.com>
2022-12-19 18:21:56 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string
+ * @str: string input
+ * Return: string
+ */
+void _puts(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != '\0'; c++)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
+}
