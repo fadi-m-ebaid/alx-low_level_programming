97a9cf90b91c2001a9071b157705a3cab4008715
fadi-m-ebaid <fadi.micheal@hotmail.com>
2023-01-05 21:08:03 +0000

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * main - prints a string followed by a new line
+ * @s: string to be printed
+ *
+ * Retunt: return nothing in success
+ */
+
+void _puts_recursion(char *s);
+{
+	if (*s == 0)
+	{
+		_putchar('\n')
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
