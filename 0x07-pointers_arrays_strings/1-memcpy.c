da98875e94974dbce5653f729cf355b147675c4e
fadi-m-ebaid <fadi.micheal@hotmail.com>
2023-01-20 07:18:34 -0800

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * *_memcpy - Copy memory area from src to the dest
+ * @src: pointer that carry the memory values of the info to be coppied
+ * @dest: pointer which will recive the info
+ * @n: the number of bytes to be coppied from src to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
